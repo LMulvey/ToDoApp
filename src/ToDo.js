581f373465adf4f738ba94c02f4a74aab63f79a2
Lee Mulvey <lmulvey@me.com>
2018-01-07 23:12:08 -0700

    Formatting.

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ToDoRow extends Component {
   render() {
-    const {summary, complete} = this.props.todo;
+    const { summary, complete } = this.props.todo;
     const index = this.props.index;
-    const checkmarkState = complete ? 'checked' : '';
-    
-    return(
+    const checkmarkState = complete ? "checked" : "";
+
+    return (
       <div class="todo-row">
-          {summary}
-          <input 
-            class="todo-checkbox"
-            type="checkbox"
-            checked={checkmarkState}
-            onChange={(e) => this.props.handleCompleted(e, index)}  />
+        {summary}
+        <input
+          class="todo-checkbox"
+          type="checkbox"
+          checked={checkmarkState}
+          onChange={e => this.props.handleCompleted(e, index)}
+        />
       </div>
     );
   }
@@ -25,53 +26,48 @@ class ToDoRowContainer extends Component {
 
     this.props.todos.forEach((todo, i) => {
       rows.push(
-        <ToDoRow 
+        <ToDoRow
           todo={todo}
           index={i}
           key={i}
-          handleCompleted={this.props.handleCompleted} />
+          handleCompleted={this.props.handleCompleted}
+        />
       );
     });
-    return (
-      <div class="todo-rows"> 
-        {rows}
-      </div>
-    );
+    return <div class="todo-rows">{rows}</div>;
   }
 }
 
 class ToDoInput extends Component {
   render() {
-    return(
+    return (
       <div class="todoInputContainer">
         <input
           class="todoInput"
           value={this.props.value}
           name="todoInputField"
-          onChange={this.props.onChange} />
+          onChange={this.props.onChange}
+        />
       </div>
     );
   }
 }
 
-
 class Button extends Component {
   render() {
     return (
-      <button 
-        class="todoInputButton"
-        onClick={this.props.onClick} >
+      <button class="todoInputButton" onClick={this.props.onClick}>
         Yes
       </button>
     );
   }
 }
 
-const TODOS = JSON.parse(localStorage.getItem('toDoList')) || [
-  {summary: 'Do the dishes', complete: true},
-  {summary: 'Laundry!!', complete: false},
-  {summary: 'Feed the fishes', complete: true},
-  {summary: 'Bees? Bees!', complete: false}
+const TODOS = JSON.parse(localStorage.getItem("toDoList")) || [
+  { summary: "Do the dishes", complete: true },
+  { summary: "Laundry!!", complete: false },
+  { summary: "Feed the fishes", complete: true },
+  { summary: "Bees? Bees!", complete: false }
 ];
 
 class ToDo extends Component {
@@ -79,8 +75,8 @@ class ToDo extends Component {
     super(props);
     this.state = {
       toDoList: TODOS,
-      toDoInput: ''
-    }
+      toDoInput: ""
+    };
 
     this._handleChange = this._handleChange.bind(this);
     this._handleClick = this._handleClick.bind(this);
@@ -88,44 +84,41 @@ class ToDo extends Component {
   }
 
   _handleChange(e) {
-    this.setState({toDoInput: e.target.value});
+    this.setState({ toDoInput: e.target.value });
   }
 
   _handleClick() {
-    this.setState((prev) => {
+    this.setState(prev => {
       const todos = [
-        ...prev.toDoList, 
+        ...prev.toDoList,
         { summary: this.state.toDoInput, complete: false }
-      ]
-      localStorage.setItem('toDoList', JSON.stringify(todos));
+      ];
+      localStorage.setItem("toDoList", JSON.stringify(todos));
       return {
         toDoList: todos,
-        toDoInput: ''
-      }
-     });
-     
+        toDoInput: ""
+      };
+    });
   }
 
   _handleCompleted(e, index) {
     let modify = this.state.toDoList;
     let completed = e.target.checked;
-    modify[index].complete = completed
-    
-    localStorage.setItem('toDoList', JSON.stringify(modify));  
-    this.setState({toDoList: modify});
+    modify[index].complete = completed;
+
+    localStorage.setItem("toDoList", JSON.stringify(modify));
+    this.setState({ toDoList: modify });
   }
 
   render() {
-    return(
+    return (
       <div class="todo-container">
-        <ToDoInput
-          onChange={this._handleChange}
-          value={this.state.toDoInput} />
-        <Button 
-          onClick={this._handleClick} />
+        <ToDoInput onChange={this._handleChange} value={this.state.toDoInput} />
+        <Button onClick={this._handleClick} />
         <ToDoRowContainer
           todos={this.state.toDoList}
-          handleCompleted={this._handleCompleted} />
+          handleCompleted={this._handleCompleted}
+        />
       </div>
     );
   }
